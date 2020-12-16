c26f8d4eda20a0e99e2943377966c088b3b694e0
charles rainey <charles.rainey@gmail.com>
2020-12-15 22:48:53 -0500

    updated file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# workdayPlannerHW5-
+# workdayPlanner
+File was created using HTML with the moment.Js attachement.
+Jquery was added to HTML file to enhance javascript tab.
+ 
--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-var time = moment().hours();
+var date = moment().format("MMM Do, YYYY");
+var display = $("#currentDay");
+display.text(date)
+console.log(date);
+console.log(display);
+ 
+var currentTime = moment().format('LT');
+console.log(currentTime);
 
-function getDate(){
-    $("#currentDay").text(moment().format("MMM Do YY"))
+function hourColor(){
+    $("textarea").each(function(){
+      var currentTime = $(this).attr("id");   
+      var currentHour = parseInt(currentTime);
+    //establish time of day
+    if (currentHour < currentTime) {
+        $(this).addClass("past");
+        } else if (currentHour == currentTime) {
+            $(this).addClass("present");
+        }else if (currentHour > currentTime) {
+            $(this).addClass("future");
+        }
+})
+$(".saveBtn").click(function(){
+    var hourInputs=$(this).siblings(".event").val;
+    var inputsRow=$(this).siblings(".event").attr("id");
+    localStorage.setItem(inputsLocation,scheduleInputs);
+    });
+
 }
